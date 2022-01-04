9bf863350674141ab278df36c2d5bdf813cfa81f
kawanji01 <kawanji01@gmail.com>
2022-01-05 01:14:39 +0900

    Add tabBar to home

---
+++ lib/models/tab_info.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class TabInfo {
+  String label;
+  Widget widget;
+  TabInfo(this.label, this.widget);
+}
---
+++ lib/widgets/home/drills_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DrillsPage extends StatelessWidget {
+  const DrillsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
