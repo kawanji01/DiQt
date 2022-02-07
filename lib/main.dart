d75002831fda7b813d64853654021f5d3390484b
kawanji01 <kawanji01@gmail.com>
2022-02-07 21:42:12 +0900

    Rename env

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'package:google_fonts/google_fonts.dart';
 
 Future main() async {
   // 環境変数を読み込む。参考： https://pub.dev/packages/flutter_dotenv
-  await dotenv.load(fileName: ".env.development");
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
