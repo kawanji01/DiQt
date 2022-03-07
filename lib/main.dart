aa2a4952a23d63292555b1c198b7a8a3ff850c32
kawanji01 <kawanji01@gmail.com>
2022-03-07 21:32:37 +0900

    Install riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-Future main() async {
-  // 環境変数を読み込む。参考： https://pub.dev/packages/flutter_dotenv
+Future<void> main() async {
+  // 環境変数の読み込み　ref： https://pub.dev/packages/flutter_dotenv
   await dotenv.load(fileName: ".env");
   // RevenueCatの初期化
   final purchase = PurchaseService();
   purchase.initPlatformState();
-  // 広告（AdMob）の初期化 ref: https://developers.google.cn/admob/flutter/quick-start?hl=ja#ios
+  // runApp()を実行する前に Flutter Engine の機能を使いたい場合に呼び出しておくおまじないコード。AdMobの初期化に必要。 ref: https://zenn.dev/sugitlab/books/flutter_poke_app_handson/viewer/step7
   WidgetsFlutterBinding.ensureInitialized();
+  // 広告（AdMob）の初期化 ref: https://developers.google.cn/admob/flutter/quick-start?hl=ja#ios
   MobileAds.instance.initialize();
-
-  runApp(const MyApp());
+  // Riverpodをアプリに適用
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     // locale : https://zenn.dev/dala/books/flutter-basic-app/viewer/create-flashcard-edit
@@ -32,7 +39,7 @@ class MyApp extends StatelessWidget {
       title: 'DiQt',
       locale: locale,
       // navigatorKey: Catcher.navigatorKey,
-      // 画面全体に被さるローディングの初期化。参考： https://pub.dev/packages/flutter_easyloading
+      // 画面全体に被さるローディングの初期化　参考： https://pub.dev/packages/flutter_easyloading
       builder: EasyLoading.init(),
       theme: ThemeData(
         primarySwatch: Colors.green,
