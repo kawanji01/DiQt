eff76f2f247f46783cc9d3dbedb93577c14de639
kawanji01 <kawanji01@gmail.com>
2022-03-08 16:07:04 +0900

    Fix purchaseService

--- lib/services/purchase.dart
+++ lib/services/purchase.dart
@@ -8,12 +8,12 @@ import 'package:purchases_flutter/purchases_flutter.dart';
 import 'package:http/http.dart' as http;
 
 // RevenueCatのセットアップ
-// 参照： https://docs.revenuecat.com/docs/getting-started-1#section-configure-purchases
+// ref： https://docs.revenuecat.com/docs/getting-started-1#section-configure-purchases
 class PurchaseService {
   // 処理実行中にコールバック（_purchaserInfoUpdated）を実行しないための処理
   bool isExecuting = false;
 
-  // RevenueCatの初期化するメソッド（呼んだ回数分コールバックが呼ばれるようになるのでMainで１度だけ実行する）。 参照：https://docs.revenuecat.com/docs/getting-started#7-configure-the-purchases-sdk
+  // RevenueCatの初期化するメソッド（呼んだ回数分コールバックが呼ばれるようになるのでMainで１度だけ実行する）。 ref：https://docs.revenuecat.com/docs/getting-started#7-configure-the-purchases-sdk
   Future<void> initPlatformState() async {
     // デバック用のログを表示する
     // await Purchases.setDebugLogsEnabled(true);
@@ -26,11 +26,11 @@ class PurchaseService {
       await Purchases.setup('$publicIosSdkKey');
     }
 
-    // コールバック時に実行し、契約状況を同期する。
+    // アプリ起動時などのコールバック時に実行され、契約状況をDBと同期する。
     Future<void> _purchaserInfoUpdated(PurchaserInfo info) async {
       print('purchaserInfoUpdated: $isExecuting');
+      print('_purchaserInfoUpdated: ${info.entitlements}');
       if (isExecuting) return;
-      print('purchaserInfoUpdated: $info');
       await syncSubscription(info);
     }
 
@@ -38,7 +38,7 @@ class PurchaseService {
     Purchases.addPurchaserInfoUpdateListener(_purchaserInfoUpdated);
   }
 
-  // RevenueCatのユーザー認証。UserSetupで利用 参考：https://docs.revenuecat.com/docs/user-ids#logging-back-in
+  // RevenueCatのユーザー認証。UserSetupで利用 　ref：https://docs.revenuecat.com/docs/user-ids#logging-back-in
   // （利用場所を検索しやすいように独自にプロパティを用意した。識別子には、public_uidを将来的にユーザーが変更できるようにする可能性も考慮して、インクリメンタルなIDを利用した。）
   static Future<void> identify(String userId) async {
     await Purchases.logIn(userId);
@@ -60,20 +60,20 @@ class PurchaseService {
     return product.identifier;
   }
 
-  // 購入処理。
-  // 購入が完了したらtrueを返す。購入がキャンセルされるかDB側の同期が失敗したらfalseを返す。
+  // 購入処理
+  // 購入が完了したらtrueを返す。購入がキャンセルされるか、DBの同期が失敗したらfalseを返す。
   Future<bool> subscribe(String productID) async {
     try {
       isExecuting = true;
-      // 購入処理
+      // クライアント側の購入処理
+      await Purchases.purchaseProduct(productID);
+      const storage = FlutterSecureStorage();
+      String? token = await storage.read(key: 'token');
+      // DB側の購入情報の同期
       // パスワードの入力の不要なキャッシュから購入（Vending PurchaserInfo from cache.）された場合、
       // syncSubscription(info)で契約処理を行なってしまうと、isActive: falseのinfoが引き渡されることによって、
       // syncSubscription(info)内のinfo.entitlements.active.isNotEmptyをすり抜けて、契約処理ではなく、解約処理が実行されてしまう。
-      // そのため、あらかじめ購入とわかっている場合には、syncSubscriptionではなく、getOrCreateSubscriberを使う。
-      // final info = await Purchases.purchaseProduct(productID);
-      // final isSubscribed = await syncSubscription(info);
-      const storage = FlutterSecureStorage();
-      String? token = await storage.read(key: 'token');
+      // そのため、あらかじめ購入とわかっている場合には、同期にはsyncSubscriptionではなく、getOrCreateSubscriberを使う。
       final isSubscribed = await getOrCreateSubscriber(token);
       return isSubscribed;
     } catch (e) {
@@ -85,11 +85,13 @@ class PurchaseService {
     }
   }
 
-  // 同期処理。
+  // DBの同期処理
+  // restoreや_purchaserInfoUpdated（コールバック）で利用
   // PurchaserInfo（購入情報）を引数にして、ユーザーの購入状況（契約中か解約済か）をDB側で同期する。
   Future<bool> syncSubscription(PurchaserInfo info) async {
     isExecuting = true;
     bool isSubscribed = false;
+    print('syncSubscription: ${info.entitlements}');
 
     const storage = FlutterSecureStorage();
     String? token = await storage.read(key: 'token');
@@ -109,10 +111,12 @@ class PurchaseService {
       print(".syncSubscription: Empty / 解約する");
       isSubscribed = await deleteSubscriber(token);
     }
+    isExecuting = false;
     return isSubscribed;
   }
 
-  // DB側の契約処理
+  // DB側の購入処理（DBとの同期）
+  // クライアント側で購入を行なっているので、これはDB側の購入の同期処理。
   // サーバー側でユーザーの契約しているサブスクを取得するか、なければ契約して、契約をDBと同期する。 ref: https://docs.revenuecat.com/reference/subscribers
   Future<bool> getOrCreateSubscriber(token) async {
     String? platform;
@@ -126,25 +130,26 @@ class PurchaseService {
         '${const String.fromEnvironment("ROOT_URL")}/api/v1/mobile/users/get_or_create_subscriber');
     var res =
         await http.post(url, body: {'token': token, 'platform': platform});
-    print('.getOrCreateSubscriber: response ${res.statusCode}');
+
     if (res.statusCode != 200) {
+      print('.getOrCreateSubscriber: response ${res.statusCode}');
       return false;
     }
 
     Map resMap = json.decode(res.body);
-    print('.getOrCreateSubscriber: ${resMap['message']}');
     await UserSetup.signIn(resMap);
     return true;
   }
 
   // DB側の解約処理
-  // サーバー側でSubscriptionを削除して、解約をDBと同期する。
+  // クライアント側に解約APIは用意されていないので、サーバー側（Ruby）の解約APIを叩き、解約をDBと同期する。
   Future<bool> deleteSubscriber(token) async {
     var url = Uri.parse(
         '${const String.fromEnvironment("ROOT_URL")}/api/v1/mobile/users/delete_subscriber');
     var res = await http.post(url, body: {'token': token});
-    print('.deleteSubscriber: response ${res.statusCode}');
+
     if (res.statusCode != 200) {
+      print('.deleteSubscriber: response ${res.statusCode}');
       return false;
     }
 
@@ -154,10 +159,10 @@ class PurchaseService {
     return true;
   }
 
-  // リストア処理。ref: https://docs.revenuecat.com/docs/restoring-purchases
+  // リストア処理 ref: https://docs.revenuecat.com/docs/restoring-purchases
   Future<bool> restore() async {
     try {
-      print('.retore');
+      isExecuting = true;
       PurchaserInfo restoredInfo = await Purchases.restoreTransactions();
       // リストアできる購入情報が存在しなければ、falseを返す。
       // if文はアクティブなサブスクが一つもなければtrueを返す処理 ref:  https://docs.revenuecat.com/docs/purchaserinfo#checking-if-a-user-is-subscribed
@@ -167,6 +172,8 @@ class PurchaseService {
     } on PlatformException catch (e) {
       print('.restore: $e');
       return false;
+    } finally {
+      isExecuting = false;
     }
   }
 }
