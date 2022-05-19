19db292b5ff0af14799d070227f13e7eb330d54f
kawanji01 <kawanji01@gmail.com>
2022-05-19 13:32:56 +0900

    Add Wordw#destroy

---
+++ lib/widgets/word/form/destroy_confirmation_screen.dart
@@ -0,0 +1,72 @@
+import 'package:booqs_mobile/data/remote/words.dart';
+import 'package:booqs_mobile/models/word.dart';
+import 'package:booqs_mobile/pages/dictionary/show.dart';
+import 'package:booqs_mobile/pages/word/show.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
+class WordFormDestroyConfirmationScreen extends StatelessWidget {
+  const WordFormDestroyConfirmationScreen({Key? key, required this.word})
+      : super(key: key);
+  final Word word;
+
+  @override
+  Widget build(BuildContext context) {
+    const heading = Text('削除確認',
+        style: TextStyle(
+            fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black87));
+
+    const explanation = Text('この項目の削除申請を行います。よろしいですか？',
+        style: TextStyle(fontSize: 16, color: Colors.black87));
+
+    Future _submit() async {
+      // 画面全体にローディングを表示
+      EasyLoading.show(status: 'loading...');
+      final Map? resMap = await RemoteWords.destroy(word.id);
+      EasyLoading.dismiss();
+      if (resMap == null) return;
+      final snackBar = SnackBar(content: Text(resMap['message']));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+      if (resMap['word'] == null) {
+        DictionaryShowPage.push(context, word.dictionaryId);
+      } else {
+        WordShowPage.push(context, word.id);
+      }
+    }
+
+    final submitButton = ElevatedButton.icon(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.red,
+        minimumSize: const Size(double.infinity,
+            40), // 親要素まで横幅を広げる。参照： https://stackoverflow.com/questions/50014342/how-to-make-button-width-match-parent
+      ),
+      onPressed: () {
+        _submit();
+      },
+      icon: const Icon(Icons.delete, color: Colors.white),
+      label: const Text(
+        '削除する',
+        style: TextStyle(
+            fontWeight: FontWeight.bold, fontSize: 18, color: Colors.white),
+      ),
+    );
+
+    return Container(
+      width: double.infinity,
+      height: 320,
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
+      child: SingleChildScrollView(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          heading,
+          const SizedBox(height: 16),
+          explanation,
+          const SizedBox(height: 32),
+          submitButton
+        ],
+      )),
+    );
+  }
+}
