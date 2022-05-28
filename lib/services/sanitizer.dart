8bfb7223cf7b2c07c899c7f5c38e99954a1c2e09
kawanji01 <kawanji01@gmail.com>
2022-05-28 14:03:14 +0900

    Fix sanitizer

--- lib/services/sanitizer.dart
+++ lib/services/sanitizer.dart
@@ -3,10 +3,10 @@ class Sanitizer {
     // テキストを、正規表現でキャプチャーして置き換える。
     // ref: https://stackoverflow.com/questions/62754405/flutter-dart-regex-replace-with-2
     // ref: https://api.flutter.dev/flutter/dart-core/String/replaceAllMapped.html
-    text = text.replaceAllMapped(RegExp(r'\[{2}(.*?)\|.*?\]{2}'), (match) {
+    text = text.replaceAllMapped(RegExp(r'\[{2}(.*?)\>.*?\]{2}'), (match) {
       return '${match.group(1)}';
     });
-    text = text.replaceAll(RegExp(r'(\[{2}|\]{2})'), '');
+    text = text.replaceAll(RegExp(r'(\[{2}>\]{2})'), '');
     return text;
   }
 }
