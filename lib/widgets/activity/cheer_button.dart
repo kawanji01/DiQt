939cdb42c5285fe210c505c4b0adceca5d9b4e60
kawanji01 <kawanji01@gmail.com>
2022-07-03 02:09:40 +0900

    Enhance Activities

--- lib/widgets/activity/cheer_button.dart
+++ lib/widgets/activity/cheer_button.dart
@@ -1,39 +1,47 @@
+import 'package:booqs_mobile/data/provider/user.dart';
 import 'package:booqs_mobile/data/remote/cheers.dart';
 import 'package:booqs_mobile/models/activity.dart';
 import 'package:booqs_mobile/models/cheer.dart';
+import 'package:booqs_mobile/models/user.dart';
 import 'package:booqs_mobile/widgets/button/small_green_button.dart';
 import 'package:booqs_mobile/widgets/button/small_outline_green_button.dart';
 import 'package:booqs_mobile/widgets/shared/loading_spinner.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ActivityCheerButton extends StatefulWidget {
+class ActivityCheerButton extends ConsumerStatefulWidget {
   const ActivityCheerButton({Key? key, required this.activity, this.cheer})
       : super(key: key);
   final Activity activity;
   final Cheer? cheer;
 
   @override
-  State<ActivityCheerButton> createState() => _ActivityCheerButtonState();
+  _ActivityCheerButtonState createState() => _ActivityCheerButtonState();
 }
 
-class _ActivityCheerButtonState extends State<ActivityCheerButton> {
+class _ActivityCheerButtonState extends ConsumerState<ActivityCheerButton> {
   Activity? _activity;
   Cheer? _cheer;
+  User? _user;
   bool _tapped = false;
 
   @override
   void initState() {
     super.initState();
-    setState(() {
-      _activity = widget.activity;
-      _cheer = widget.cheer;
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      setState(() {
+        _activity = widget.activity;
+        _cheer = widget.cheer;
+        _user = ref.watch(currentUserProvider);
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
     if (_activity == null) return const LoadingSpinner();
+    if (_user?.id == _activity?.user?.id) return Container();
 
     Future<void> _createCheer() async {
       final Map? resMap = await RemoteCheers.create(_activity!.id);
---
+++ lib/widgets/activity/first_of_the_day.dart
@@ -0,0 +1,97 @@
+import 'package:booqs_mobile/models/activity.dart';
+import 'package:booqs_mobile/models/user.dart';
+import 'package:booqs_mobile/widgets/user/feed_icon.dart';
+import 'package:flutter/material.dart';
+
+class ActivityFirstOfTheDay extends StatelessWidget {
+  const ActivityFirstOfTheDay({Key? key, required this.activity})
+      : super(key: key);
+  final Activity activity;
+
+  @override
+  Widget build(BuildContext context) {
+    final User user = activity.user!;
+
+    const TextStyle textBlack = TextStyle(
+        color: Colors.black87, fontSize: 16, fontWeight: FontWeight.normal);
+
+    const TextStyle textGreen = TextStyle(
+        color: Colors.green, fontSize: 16, fontWeight: FontWeight.bold);
+
+    final Widget answerDaysCount = RichText(
+        text: TextSpan(children: [
+      TextSpan(text: user.name, style: textGreen),
+      const TextSpan(text: 'が', style: textBlack),
+      TextSpan(text: '${activity.amount}日', style: textGreen),
+      const TextSpan(text: '解答しました！', style: textBlack),
+    ]));
+
+    Widget _continuationCount() {
+      if (activity.continuationCount! < 2) {
+        return Container();
+      }
+      return RichText(
+          text: TextSpan(children: [
+        TextSpan(text: '${activity.continuationCount}日連続', style: textGreen),
+        const TextSpan(text: 'で解答しました！', style: textBlack),
+      ]));
+    }
+
+    Widget _allWeekContinuationCount() {
+      if (activity.allWeekContinuationCount == null) {
+        return Container();
+      }
+      return RichText(
+          text: TextSpan(children: [
+        TextSpan(
+            text: '${activity.allWeekContinuationCount}週間連続', style: textGreen),
+        const TextSpan(text: 'で解答しました！', style: textBlack),
+      ]));
+    }
+
+    Widget _allMonthContinuationCount() {
+      if (activity.allMonthContinuationCount == null) {
+        return Container();
+      }
+      return RichText(
+          text: TextSpan(children: [
+        TextSpan(
+            text: '${activity.allMonthContinuationCount}ヶ月連続',
+            style: textGreen),
+        const TextSpan(text: 'で解答しました！', style: textBlack),
+      ]));
+    }
+
+    Widget _allYearContinuationCount() {
+      if (activity.allYearContinuationCount == null) {
+        return Container();
+      }
+      return RichText(
+          text: TextSpan(children: [
+        TextSpan(
+            text: '${activity.allYearContinuationCount}年間連続', style: textGreen),
+        const TextSpan(text: 'で解答しました！', style: textBlack),
+      ]));
+    }
+
+    return Container(
+      padding: const EdgeInsets.only(top: 16, bottom: 24),
+      child: Row(
+        children: [
+          UserFeedIcon(user: user),
+          Expanded(
+            child: Column(
+              children: [
+                answerDaysCount,
+                _continuationCount(),
+                _allWeekContinuationCount(),
+                _allMonthContinuationCount(),
+                _allYearContinuationCount()
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
