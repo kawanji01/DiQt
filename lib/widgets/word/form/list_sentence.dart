d26c246a2adce1631b37dc897c726bb1012fa7a9
kawanji01 <kawanji01@gmail.com>
2022-08-09 21:30:24 +0900

    Fix SentenceSearch on WordForm

--- lib/widgets/word/form/list_sentence.dart
+++ lib/widgets/word/form/list_sentence.dart
@@ -1,7 +1,9 @@
 import 'package:booqs_mobile/models/sentence.dart';
 import 'package:booqs_mobile/models/sentence_source.dart';
 import 'package:booqs_mobile/widgets/button/small_green_button.dart';
+import 'package:booqs_mobile/widgets/markdown/markdown_with_dict_link.dart';
 import 'package:booqs_mobile/widgets/sentence/edit_button.dart';
+import 'package:booqs_mobile/widgets/shared/text_with_dict_link.dart';
 import 'package:flutter/material.dart';
 
 class WordFormListSentence extends StatelessWidget {
@@ -28,9 +30,16 @@ class WordFormListSentence extends StatelessWidget {
       child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(sentence.original,
-                style: const TextStyle(
-                    fontSize: 16, height: 1.6, color: Colors.black87)),
+            TextWithDictLink(
+                text: sentence.original,
+                langNumber: sentence.langNumberOfOriginal,
+                autoLinkEnabled: true,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                dictionaryId: sentence.dictionaryId,
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+                fontColor: Colors.green,
+                selectable: true),
             const SizedBox(height: 8),
             Text(sentence.translation,
                 style: const TextStyle(
