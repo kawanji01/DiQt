16c5d2c9a43bf8a10d960919a1ad84e124a73308
Kawanji (Shinji Aikawa) <44082240+kawanji01@users.noreply.github.com>
2023-03-27 23:05:47 +0900

    Add posTagScreen

--- lib/components/word/item/pos_tag.dart
+++ lib/components/word/item/pos_tag.dart
@@ -1,9 +1,8 @@
 import 'package:booqs_mobile/components/shared/item_label.dart';
+import 'package:booqs_mobile/components/word/item/pos_tag_screen.dart';
 import 'package:booqs_mobile/models/pos_tag.dart';
 import 'package:booqs_mobile/models/word.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class WordItemPosTag extends StatelessWidget {
   const WordItemPosTag({Key? key, required this.word}) : super(key: key);
@@ -15,6 +14,20 @@ class WordItemPosTag extends StatelessWidget {
     if (posTag == null) {
       return Container();
     }
-    return SharedItemLabel(text: posTag.name);
+    return InkWell(
+      child: SharedItemLabel(text: posTag.name),
+      onTap: () {
+        showModalBottomSheet(
+          isScrollControlled: true,
+          context: context,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15.0),
+                topRight: Radius.circular(15.0)),
+          ),
+          builder: (context) => WordItemPosTagScreen(posTag: posTag),
+        );
+      },
+    );
   }
 }
