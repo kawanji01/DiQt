761a46953242e182646684a177a9813ef10a9762
kawanji01 <kawanji01@gmail.com>
2023-06-04 07:49:18 +0900

    Fix activities

--- lib/data/remote/activities.dart
+++ lib/data/remote/activities.dart
@@ -1,29 +1,26 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-import 'package:booqs_mobile/utils/crashlytics_service.dart';
 import 'package:booqs_mobile/utils/diqt_url.dart';
+import 'package:booqs_mobile/utils/error_handler.dart';
 import 'package:booqs_mobile/utils/http_service.dart';
 import 'package:http/http.dart';
 
 class RemoteActivities {
-  static Future<Map?> index(int pageKey, int pageSize, String order) async {
+  static Future<Map> index(int pageKey, int pageSize, String order) async {
     try {
       final Uri url = Uri.parse(
           '${DiQtURL.root()}/api/v1/mobile/activities/list?order=$order&page=$pageKey&size=$pageSize');
       final Response res = await HttpService.get(url);
-      if (res.statusCode != 200) return null;
+      if (ErrorHandler.isErrorResponse(res)) return ErrorHandler.errorMap(res);
       final Map resMap = json.decode(res.body);
       return resMap;
     } on TimeoutException catch (e, s) {
-      CrashlyticsService.reccordError(e, s);
-      return null;
+      return ErrorHandler.timeoutMap(e, s);
     } on SocketException catch (e, s) {
-      CrashlyticsService.reccordError(e, s);
-      return null;
+      return ErrorHandler.socketExceptionMap(e, s);
     } catch (e, s) {
-      CrashlyticsService.reccordError(e, s);
-      return null;
+      return ErrorHandler.exceptionMap(e, s);
     }
   }
 }
