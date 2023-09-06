6f286fae720cad2e37bcfd0a5531f9782eea3193
kawanji01 <kawanji01@gmail.com>
2023-09-06 09:07:03 +0900

    Modify dictionaryPage when locale is en

--- lib/components/home/dictionary_screen.dart
+++ lib/components/home/dictionary_screen.dart
@@ -5,6 +5,7 @@ import 'package:booqs_mobile/components/dictionary_map/search_screen.dart';
 import 'package:booqs_mobile/components/user/lang_init_screen.dart';
 import 'package:booqs_mobile/data/provider/current_user.dart';
 import 'package:booqs_mobile/data/provider/dictionary_map.dart';
+import 'package:booqs_mobile/data/provider/locale.dart';
 import 'package:booqs_mobile/i18n/translations.g.dart';
 import 'package:booqs_mobile/models/tab_info.dart';
 import 'package:booqs_mobile/models/user.dart';
@@ -47,6 +48,8 @@ class _HomeDictionaryScreenState extends ConsumerState<HomeDictionaryScreen> {
     //
     final String dictionary = t.layouts.dictionary;
     final String wordList = t.layouts.word_list;
+    final String locale = ref.watch(localeProvider);
+
     final List<TabInfo> tabs = [
       TabInfo(dictionary, const DictionaryMapSearchScreen()),
       TabInfo(wordList, const ChapterIndex()),
@@ -61,53 +64,70 @@ class _HomeDictionaryScreenState extends ConsumerState<HomeDictionaryScreen> {
       ];
     }
 
-    //
-    return DefaultTabController(
-      initialIndex: 0,
-      length: 2,
-      child: Builder(
-        builder: (BuildContext context) {
-          final TabController tabController = DefaultTabController.of(context);
-          tabController.addListener(() {
-            // この条件式がないとタブ移動時に2回実行される。
-            if (!tabController.indexIsChanging) {
-              if (tabController.index == 0) {
-                ref
-                    .read(dictionaryMapFloatingActionButtonProvider.notifier)
-                    .state = const DictionaryMapFloatingActionButton();
-              } else {
-                // tabIndexが0でないときは、FloatingActionButtonを表示しない & キーボードを閉じる
-                FocusScope.of(context).unfocus();
-                ref
-                    .read(dictionaryMapFloatingActionButtonProvider.notifier)
-                    .state = null;
+    if (locale == 'ja') {
+      return DefaultTabController(
+        initialIndex: 0,
+        length: 2,
+        child: Builder(
+          builder: (BuildContext context) {
+            final TabController tabController =
+                DefaultTabController.of(context);
+            tabController.addListener(() {
+              // この条件式がないとタブ移動時に2回実行される。
+              if (!tabController.indexIsChanging) {
+                if (tabController.index == 0) {
+                  ref
+                      .read(dictionaryMapFloatingActionButtonProvider.notifier)
+                      .state = const DictionaryMapFloatingActionButton();
+                } else {
+                  // tabIndexが0でないときは、FloatingActionButtonを表示しない & キーボードを閉じる
+                  FocusScope.of(context).unfocus();
+                  ref
+                      .read(dictionaryMapFloatingActionButtonProvider.notifier)
+                      .state = null;
+                }
               }
-            }
-          });
-          return Scaffold(
-              appBar: AppBar(
-                automaticallyImplyLeading: false,
-                // タイトル部分を消す ref: https://blog.mrym.tv/2019/09/flutter-tabbar-without-appbar-title/
-                flexibleSpace: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    TabBar(isScrollable: true, tabs: tabBars()),
-                  ],
+            });
+            return Scaffold(
+                appBar: AppBar(
+                  automaticallyImplyLeading: false,
+                  // タイトル部分を消す ref: https://blog.mrym.tv/2019/09/flutter-tabbar-without-appbar-title/
+                  flexibleSpace: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      TabBar(isScrollable: true, tabs: tabBars()),
+                    ],
+                  ),
                 ),
-              ),
-              body: Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: ResponsiveValues.horizontalMargin(context),
+                body: Container(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: ResponsiveValues.horizontalMargin(context),
+                  ),
+                  child: TabBarView(
+                      children: tabs.map((tab) => tab.widget).toList()),
                 ),
-                child: TabBarView(
-                    children: tabs.map((tab) => tab.widget).toList()),
-              ),
-              bottomNavigationBar: const BottomNavbar(),
-              floatingActionButton: ref.watch(
-                  dictionaryMapFloatingActionButtonProvider) // tabIndexが0でないときは、FloatingActionButtonを表示しない
-              );
-        },
-      ),
-    );
+                bottomNavigationBar: const BottomNavbar(),
+                floatingActionButton: ref.watch(
+                    dictionaryMapFloatingActionButtonProvider) // tabIndexが0でないときは、FloatingActionButtonを表示しない
+                );
+          },
+        ),
+      );
+    }
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(dictionary),
+          automaticallyImplyLeading: false,
+        ),
+        body: Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: ResponsiveValues.horizontalMargin(context),
+          ),
+          child: const DictionaryMapSearchScreen(),
+        ),
+        bottomNavigationBar: const BottomNavbar(),
+        floatingActionButton: ref.watch(
+            dictionaryMapFloatingActionButtonProvider) // tabIndexが0でないときは、FloatingActionButtonを表示しない
+        );
   }
 }
