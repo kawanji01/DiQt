2a9aae7c9767d99bfc6c7f329b8e32f6884f0a71
Shinji Aikawa(kawanji) <kawanji01@gmail.com>
2024-04-20 12:52:24 +0900

    Add clearImageUrl

--- lib/models/drill.dart
+++ lib/models/drill.dart
@@ -15,6 +15,7 @@ class Drill {
       required this.introduction,
       this.referenceUrl,
       required this.thumbnailUrl,
+      required this.clearImageUrl,
       required this.publicUid,
       required this.strictSolvingMode,
       required this.answerHistoriesCount,
@@ -36,6 +37,7 @@ class Drill {
   String introduction;
   String? referenceUrl;
   String thumbnailUrl;
+  String clearImageUrl;
   String publicUid;
   bool strictSolvingMode;
   DateTime createdAt;
@@ -58,6 +60,8 @@ class Drill {
         introduction = json['introduction'] ?? '',
         referenceUrl = json['reference_url'] ?? '',
         thumbnailUrl = json['thumbnail_url'] ?? '',
+        clearImageUrl = json['clear_image_url'] ??
+            'https://res.cloudinary.com/hkbyf3jop/image/upload/w_1200,h_620/a_345,l_all_solved_stamp/v1663380933/DiQt_no_image.png',
         publicUid = json['public_uid'],
         strictSolvingMode = json['strict_solving_mode'],
         createdAt = DateTime.parse(json['created_at']),
@@ -85,6 +89,7 @@ class Drill {
         'introduction': introduction,
         'reference_url': referenceUrl,
         'thumbnail_url': thumbnailUrl,
+        'clear_image_url': clearImageUrl,
         'public_uid': publicUid,
         'strict_solving_mode': strictSolvingMode,
         'created_at': createdAt,
