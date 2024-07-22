41a26f439a4882a40940774d4dcfc315aa731967
Shinji Aikawa(kawanji) <kawanji01@gmail.com>
2024-07-22 21:23:43 +0900

    Add audioPlayerProvider

--- lib/components/shared/audio_button.dart
+++ lib/components/shared/audio_button.dart
@@ -1,47 +1,15 @@
-import 'package:audioplayers/audioplayers.dart';
+import 'package:booqs_mobile/data/provider/audio_player.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AudioButton extends StatefulWidget {
+class AudioButton extends ConsumerWidget {
   const AudioButton({super.key, required this.url});
   final String url;
 
   @override
-  State<AudioButton> createState() => _AudioButtonState();
-}
+  Widget build(BuildContext context, WidgetRef ref) {
+    final audioPlayerNotifier = ref.read(audioPlayerNotifierProvider.notifier);
 
-class _AudioButtonState extends State<AudioButton> {
-  late AudioPlayer _audioPlayer;
-
-  @override
-  void initState() {
-    super.initState();
-    // Create the audio player.
-    _audioPlayer = AudioPlayer();
-
-    // Set the release mode to keep the source after playback has completed.
-    _audioPlayer.setReleaseMode(ReleaseMode.stop);
-
-    // Preload the audio source.
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      try {
-        await _audioPlayer.setSourceUrl(widget.url);
-        // Androidだと一度しか音声が再生されなくなるのでコメントアウト
-        // ref; https://pub.dev/documentation/audioplayers/latest/audioplayers/PlayerMode.html
-        // await _audioPlayer.setPlayerMode(PlayerMode.lowLatency);
-      } catch (e) {
-        print('Error setting audio source: $e');
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _audioPlayer.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
         foregroundColor: Colors.white,
@@ -57,10 +25,9 @@ class _AudioButtonState extends State<AudioButton> {
       ),
       onPressed: () async {
         try {
-          // Seek to the beginning before playing.
-          await _audioPlayer.seek(Duration.zero);
-          // Use resume instead of play to continue from the start.
-          await _audioPlayer.resume();
+          // 音声を再生
+          await audioPlayerNotifier.setSourceUrl(url);
+          await audioPlayerNotifier.play();
         } catch (e) {
           print('Error playing audio: $e');
         }
