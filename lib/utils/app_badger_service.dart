fe1a692427e3fab7592ba5cdbf336e5374542076
Shinji Aikawa(kawanji) <kawanji01@gmail.com>
2025-02-19 16:32:45 +0900

    Introduce FlutterAppBadgeControl

--- lib/utils/app_badger_service.dart
+++ lib/utils/app_badger_service.dart
@@ -2,13 +2,14 @@ import 'dart:convert';
 
 import 'package:booqs_mobile/utils/crashlytics_service.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_app_badge_control/flutter_app_badge_control.dart';
 
 class AppBadgerService {
   //// 復習のバッジつける処理 ////
   static Future<void> syncReviewBadgeOnPushNotification() async {
-    // if (await FlutterAppBadger.isAppBadgeSupported() == false) {
-    //  return;
-    // }
+    if (await FlutterAppBadgeControl.isAppBadgeSupported() == false) {
+      return;
+    }
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
       // print('FirebaseMessaging.onMessage.listen: ${message.notification?.title ?? ''}');
 
@@ -22,9 +23,8 @@ class AppBadgerService {
       try {
         final Map<String, dynamic> contentsJson =
             jsonDecode(message.data['contents']);
-        // TODO: ここでバッジの数を指定する。flutter_app_badger がdiscontinuedなので、代替案を探す。
         final int reviewsCount = contentsJson['reviews_count'] ?? 0;
-        // FlutterAppBadger.updateBadgeCount(reviewsCount); //バッジの数を指定
+        updateReviewBadge(reviewsCount); //バッジの数を指定
       } catch (e, str) {
         CrashlyticsService.recordError(e, str);
       }
@@ -35,15 +35,15 @@ class AppBadgerService {
   // バッジを更新する
   static Future<void> updateReviewBadge(int unsolvedReviewsCount) async {
     try {
-      // if (await FlutterAppBadger.isAppBadgeSupported() == false) {
-      //  return;
-      // }
+      if (await FlutterAppBadgeControl.isAppBadgeSupported() == false) {
+        return;
+      }
 
-      // if (unsolvedReviewsCount == 0) {
-      //  FlutterAppBadger.removeBadge(); //バッジを削除
-      // } else {
-      //  FlutterAppBadger.updateBadgeCount(unsolvedReviewsCount);
-      //}
+      if (unsolvedReviewsCount == 0) {
+        FlutterAppBadgeControl.removeBadge(); //バッジを削除
+      } else {
+        FlutterAppBadgeControl.updateBadgeCount(unsolvedReviewsCount);
+      }
     } catch (e, str) {
       CrashlyticsService.recordError(e, str);
     }
