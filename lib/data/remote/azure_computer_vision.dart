7b7c8d2efc2e2259ac5cdc04d9226e33f4929d32
Shinji Aikawa(kawanji) <kawanji01@gmail.com>
2025-05-16 19:28:14 +0900

    Android OCRの修正

--- lib/data/remote/azure_computer_vision.dart
+++ lib/data/remote/azure_computer_vision.dart
@@ -5,34 +5,54 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
 class AzureComputerVision {
-  // 画像からテキストを取得する
-  static Future<String?> analyzeImageBinary(
-      {required File imageData, required String language}) async {
+  // 画像からテキストを取得
+  static Future<String?> analyzeImageBinary({
+    required File imageData,
+    required String language, // 例: 'ja'
+  }) async {
     try {
-      final String? endpoint = dotenv.env['AZURE_COMPUTER_VISION_ENDPOINT'];
-      final String? subscriptionKey =
-          dotenv.env['AZURE_COMPUTER_VISION_SUBSCRIPTION_KEY'];
+      final endpoint = dotenv.env['AZURE_COMPUTER_VISION_ENDPOINT']; // ← 新リソース
+      final key = dotenv.env['AZURE_COMPUTER_VISION_SUBSCRIPTION_KEY'];
 
-      final Uri url = Uri.parse(
-          '$endpoint/computervision/imageanalysis:analyze?api-version=2023-02-01-preview&features=read&language=$language');
-      final Map<String, String> headers = {
-        'Ocp-Apim-Subscription-Key': '$subscriptionKey',
-        'Content-Type': 'application/octet-stream'
-      };
+      final url = Uri.parse(
+        '$endpoint/computervision/imageanalysis:analyze'
+        '?api-version=2023-10-01'
+        '&features=read'
+        '&language=$language',
+      );
+
       final res = await http.post(
         url,
-        headers: headers,
-        body: imageData.readAsBytesSync(),
+        headers: {
+          'Ocp-Apim-Subscription-Key': key!,
+          'Content-Type': 'application/octet-stream',
+        },
+        body: await imageData.readAsBytes(),
       );
-      final Map resMap = json.decode(res.body);
-      final String text = resMap['readResult']['content'];
+
+      if (res.statusCode != 200) throw 'HTTP ${res.statusCode}: ${res.body}';
+
+      final jsonBody = json.decode(res.body) as Map<String, dynamic>;
+      print(jsonBody);
+
+      if (jsonBody['readResult'] == null) {
+        throw 'Failed to analyze image: ${jsonBody.toString()}';
+      }
+      final Map<String, dynamic> readResult = jsonBody['readResult'];
+
+      // blocksから全てのテキストを取得して結合
+      final List<dynamic> blocks = readResult['blocks'] as List<dynamic>;
+      print(blocks);
+      final String text = blocks
+          .expand((block) => (block['lines'] as List<dynamic>))
+          .map((line) => line['text'] as String)
+          .join('');
+
       return text;
-    } on TimeoutException {
-      return null;
     } on SocketException {
-      return null;
+      return null; // ネットワークエラー
     } catch (e) {
-      return null;
+      return null; // その他エラー
     }
   }
 }
